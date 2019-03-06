2b0b4708e985426eda5c6c3453679bad17d7af66
mitchy <yourstonmj@gmail.com>
2019-03-05 22:55:45 -0800

    Fixed Kepler graph with correct error cuts, added respective col's to CSV

--- BP_RP vs. AbsGmag/McQuillan/KeplerPlot.py
+++ BP_RP vs. AbsGmag/McQuillan/KeplerPlot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 Kepler = panda.read_csv("../McQuillan/keplerMcQuillan.csv")
 
-Kepler = Kepler[(Kepler.phot_g_mean_flux_error <= 50) & (Kepler.phot_rp_mean_flux_error <= 20) & (Kepler.phot_bp_mean_flux_error<= 20)]
+Kepler = Kepler[(Kepler.phot_g_mean_flux_over_error >= 50) & (Kepler.phot_rp_mean_flux_over_error >= 20) & (Kepler.phot_bp_mean_flux_over_error >= 20)]
 
 plt.plot(Kepler[["bp_rp"]], Kepler[["abs_gmag"]], marker = "v", markersize = 0.5, color = "green", linestyle = "none", label = "Kepler")
 plt.ylabel("absolute gmag")
