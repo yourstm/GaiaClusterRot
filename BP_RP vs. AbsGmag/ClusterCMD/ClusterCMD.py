91a5d960f8e0a4319fc7657f9833ee972577d12c
mitchy <yourstonmj@gmail.com>
2019-03-13 16:49:21 -0700

    Edited .py file to reflect whats in the jupyter notebook

--- BP_RP vs. AbsGmag/ClusterCMD/ClusterCMD.py
+++ BP_RP vs. AbsGmag/ClusterCMD/ClusterCMD.py
@@ -25,7 +25,7 @@ set_matplotlib_formats('pdf', 'png')
 plt.rcParams['savefig.dpi'] = 200
 
 plt.rcParams['figure.autolayout'] = False
-plt.rcParams['figure.figsize'] = 6,6
+plt.rcParams['figure.figsize'] = 8,6
 plt.rcParams['axes.labelsize'] = 18
 plt.rcParams['axes.titlesize'] = 20
 plt.rcParams['font.size'] = 16
@@ -38,15 +38,19 @@ mp.rcParams['ytick.direction'] = 'out'
 
 
 # Plotting
-plt.plot(Hyades[["bp_rp"]], Hyades[["AbsGmag"]], marker = "v", markersize = 0.5, color = "red", linestyle = "none", label = "Hyades")
-plt.plot(NGC188[["bp_rp"]], NGC188[["AbsGmag"]], marker = "v", markersize = 0.5, color = "blue", linestyle = "none", label = "NGC188")
-plt.plot(NGC752[["bp_rp"]], NGC752[["AbsGmag"]], marker = "v", markersize = 0.5, color = "green", linestyle = "none", label = "NGC752")
-plt.plot(NGC2682[["bp_rp"]], NGC2682[["AbsGmag"]], marker = "v", markersize = 0.5, color = "purple", linestyle = "none", label = "NGC2682")
-plt.plot(Praesepe[["bp_rp"]], Praesepe[["AbsGmag"]], marker = "v", markersize = 0.5, color = "orange", linestyle = "none", label = "Praesepe")
-plt.plot(Pleiades[["bp_rp"]], Pleiades[["AbsGmag"]], marker = "v", markersize = 0.5, color = "black", linestyle = "none", label = "Pleiades")
+plt.plot(Hyades[["bp_rp"]], Hyades[["AbsGmag"]]-6, marker = "v", markersize = 0.5, color = "red", linestyle = "none", label = "Hyades")
+plt.plot(NGC188[["bp_rp"]], NGC188[["AbsGmag"]]-6, marker = "v", markersize = 0.5, color = "blue", linestyle = "none", label = "NGC188")
+plt.plot(NGC752[["bp_rp"]], NGC752[["AbsGmag"]]-6, marker = "v", markersize = 0.5, color = "green", linestyle = "none", label = "NGC752")
+plt.plot(NGC2682[["bp_rp"]], NGC2682[["AbsGmag"]]-6, marker = "v", markersize = 0.5, color = "purple", linestyle = "none", label = "NGC2682")
+plt.plot(Praesepe[["bp_rp"]], Praesepe[["AbsGmag"]]-6, marker = "v", markersize = 0.5, color = "orange", linestyle = "none", label = "Praesepe")
+plt.plot(Pleiades[["bp_rp"]], Pleiades[["AbsGmag"]]-6, marker = "v", markersize = 0.5, color = "black", linestyle = "none", label = "Pleiades")
 
-plt.ylabel("AbsGmag")
-plt.xlabel("bp_rp")
+plt.ylabel("$M_G$")
+plt.xlabel("$G_{BP} - G_{RP}$")
 plt.legend(markerscale = 8)
-plt.ylim(27, 5)
+plt.xlim(-0.75, 4.25)
+plt.xticks([0, 1, 2, 3, 4])
+plt.ylim(16, -4)
+plt.yticks([0, 5, 10])
 plt.show()
+
